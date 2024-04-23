606543581876389e5ef83b9327a5960061572125
Luis Felipe Tavera <luis.tavera@newshore.es>
2024-04-22 19:54:45 -0500

    header component

--- header-container/header-container.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'header-container',
-  templateUrl: './header-container.component.html'
-})
-export class HeaderContainerComponent implements OnInit {
-  
-}
--- header-container/header-web/header-web.component.ts
+++ header-container/header-web/header-web.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'header-web',
+  standalone: true,
   templateUrl: './header-web.component.html'
 })
 export class HeaderWebComponent {
