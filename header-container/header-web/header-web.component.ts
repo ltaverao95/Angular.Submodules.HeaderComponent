3a4d69b48e71a14339b00c8e6ce035de532675d4
natalia.tavera <natalia.tavera@flyrlabs.com>
2024-04-24 07:11:44 -0500

    Title and color title was added

--- header-container/header-web/header-web.component.ts
+++ header-container/header-web/header-web.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'header-web',
+  selector: "header-web",
   standalone: true,
-  templateUrl: './header-web.component.html'
+  templateUrl: "./header-web.component.html",
+  styles: [
+    `
+      .header {
+        &__title {
+          color: pink;
+        }
+      }
+    `,
+  ],
 })
-export class HeaderWebComponent {
-  
-}
+export class HeaderWebComponent {}
